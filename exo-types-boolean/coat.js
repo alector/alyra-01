e66e3683a14ee2912b7db55672274908a2c2b457
alector <panagiotispp@gmail.com>
2021-02-15 15:44:13 +0100

    add temperature to coat.js

---
+++ exo-types-boolean/coat.js
@@ -0,0 +1,9 @@
+let isRainy = true
+let isWindy = false
+let temperature = 20
+
+if ((isRainy || isWindy) && temperature < 15) {
+  console.log('Mettez votre manteau')
+} else {
+  console.log('Nous n avez pas besoin de manteau today')
+}
