3c946856be3ea51ccbfe3a49ff485f8a70130259
alector <panagiotispp@gmail.com>
2021-02-16 19:04:01 +0100

    add let

--- exo-numbers-strings/decimal.js
+++ exo-numbers-strings/decimal.js
@@ -1,18 +1,18 @@
-n1 = 0x123 // HexIntegerLiteral
-n2 = 0123 // OctalIntegerLiteral
-n3 = 0b10011001 // BinaryIntegerLiteral
-n4 = 0xdeadbeef // HexIntegerLiteral
-n5 = 0xea7beef // HexIntegerLiteral
-n6 = 0b1111111111111111 // BinaryIntegerLiteral
-n7 = 0777 // OctalIntegerLiteral
+let n1 = 0x123 // HexIntegerLiteral
+let n2 = 0123 // OctalIntegerLiteral
+let n3 = 0b10011001 // BinaryIntegerLiteral
+let n4 = 0xdeadbeef // HexIntegerLiteral
+let n5 = 0xea7beef // HexIntegerLiteral
+let n6 = 0b1111111111111111 // BinaryIntegerLiteral
+let n7 = 0777 // OctalIntegerLiteral
 
-d1 = n1.toString()
-d2 = n2.toString()
-d3 = n3.toString()
-d4 = n4.toString()
-d5 = n5.toString()
-d6 = n6.toString()
-d7 = n7.toString()
+let d1 = n1.toString()
+let d2 = n2.toString()
+let d3 = n3.toString()
+let d4 = n4.toString()
+let d5 = n5.toString()
+let d6 = n6.toString()
+let d7 = n7.toString()
 
 msg = `
 CONVERSION
