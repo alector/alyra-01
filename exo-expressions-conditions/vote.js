342a1b5e75d94e79dc981283f9de12e98d00a9d8
alector <panagiotispp@gmail.com>
2021-02-25 19:09:38 +0100

    exos

---
+++ exo-expressions-conditions/vote.js
@@ -0,0 +1,8 @@
+let age = 17
+const canVote = age >= 18 ? true : false
+
+if (canVote) {
+  console.log('You can vote')
+} else {
+  console.log('You can not vote')
+}
---
+++ exo-expressions-conditions/welcome.js
@@ -0,0 +1,6 @@
+nom = 'Bob'
+genre = 'male'
+
+const msg = 'male' ? `Bonjour ${nom}, vous êtes entré sur le chan` : `Bonjour ${nom}, vous êtes entrée sur le chan`
+
+console.log(msg)
