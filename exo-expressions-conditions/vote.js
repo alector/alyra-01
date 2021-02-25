78a95b00b806cde902bf60b8f20248abd0d112b3
alector <panagiotispp@gmail.com>
2021-02-25 19:25:30 +0100

    add msg

--- exo-expressions-conditions/vote.js
+++ exo-expressions-conditions/vote.js
@@ -1,8 +1,5 @@
 let age = 17
 const canVote = age >= 18 ? true : false
+const msg = canVote ? 'You can vote' : 'You can not vote'
 
-if (canVote) {
-  console.log('You can vote')
-} else {
-  console.log('You can not vote')
-}
+console.log(msg)
