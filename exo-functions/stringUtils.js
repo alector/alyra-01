4178e16d039132e9d3782046e73601a941f529bd
alector <panagiotispp@gmail.com>
2021-03-03 17:46:44 +0100

    reverseStr ok

---
+++ exo-functions/stringUtils.js
@@ -0,0 +1,7 @@
+// Ecrire une fonction reverseStr qui prend en paramètre une string et qui retourne cette string inversé. Si le paramètre passé est 'Hello!' la fonction devra retourné '!olleH'
+
+const reverseStr = str => {
+  return str.split('').reverse().join('')
+}
+
+console.log(reverseStr('Hello!'))
