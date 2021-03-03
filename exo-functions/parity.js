d1fa032f949ba51eea107427194c0992383326ae
alector <panagiotispp@gmail.com>
2021-03-03 17:41:21 +0100

    isOdd ok

---
+++ exo-functions/parity.js
@@ -0,0 +1,8 @@
+// Ecrire une fonction isOdd qui prend un nombre en paramètre et qui devra retourner true si le nombre passé en paramètre est impair, sinon la fonction retournera false.
+
+const isOdd = num => {
+  return num % 2
+}
+
+console.log(isOdd(2))
+console.log(isOdd(3))
