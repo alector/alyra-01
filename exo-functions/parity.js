9a1a2358ab9526859bbb973342283d4c6818a6a1
alector <panagiotispp@gmail.com>
2021-03-03 17:43:48 +0100

    isEven ok

--- exo-functions/parity.js
+++ exo-functions/parity.js
@@ -1,8 +1,18 @@
 // Ecrire une fonction isOdd qui prend un nombre en paramètre et qui devra retourner true si le nombre passé en paramètre est impair, sinon la fonction retournera false.
 
 const isOdd = num => {
-  return num % 2
+  return Boolean(num % 2)
 }
 
 console.log(isOdd(2))
 console.log(isOdd(3))
+
+// Ajouter au fichier précédent une fonction isEven qui prend un nombre en paramètre et qui devra retourner true si le nombre passé en paramètre est pair, sinon la fonction retournera false.
+// la fonction isEven devra absolument utiliser la fonction isOdd pour effectuer sa vérification de parité.
+
+const isEven = num => {
+  return !isOdd(num)
+}
+
+console.log(isEven(2))
+console.log(isEven(3))
